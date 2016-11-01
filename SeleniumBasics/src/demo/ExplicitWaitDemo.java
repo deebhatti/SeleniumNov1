744cde5779d92d40b319a1dfdeec9a69867143af
Deepinder <DSB@DSB-PC>
2016-11-01 18:34:36 +0530

    Made a comment to reflect in Github

--- SeleniumBasics/src/demo/ExplicitWaitDemo.java
+++ SeleniumBasics/src/demo/ExplicitWaitDemo.java
@@ -19,6 +19,7 @@ public class ExplicitWaitDemo {
 		driver.get("http://the-internet.herokuapp.com/dynamic_loading/1");
 		
 		driver.findElement(By.xpath("//*[@id='start']/button")).click();
+		//Adding a comment to see in github
 		
 //		WebDriverWait wait = new WebDriverWait(driver,10);
 //		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='finish']/h4")));
